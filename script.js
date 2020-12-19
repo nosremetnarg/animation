1a8249a5f0bac1994064ca98ebf5c40448f5e746
Grant Emerson <grant.alex.emerson@gmail.com>
2020-12-19 14:40:52 -0600

    updated the correct font

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const logo = document.querySelectorAll('#logo path');
-console.log(logo);
+console.log(logo.length);
 for (let i = 0; i < logo.length; i++) {
     console.log(`Letter ${i + 1} is ${logo[i].getTotalLength()}`);
+}
+
+const logo2 = document.querySelectorAll('#logo2 path');
+console.log(logo2.length);
+
+for (let i = 0; i < logo2.length; i++) {
+    console.log(`Letter ${i + 1} is ${logo2[i].getTotalLength()}`);
 }
