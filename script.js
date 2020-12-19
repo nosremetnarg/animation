bcbe2a7b9bfe1f005e23e05f02d117367a9d8ea7
Grant Emerson <grant.alex.emerson@gmail.com>
2020-12-19 14:43:30 -0600

    console logs taken out

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const logo = document.querySelectorAll('#logo path');
-console.log(logo.length);
+// console.log(logo.length);
 for (let i = 0; i < logo.length; i++) {
-    console.log(`Letter ${i + 1} is ${logo[i].getTotalLength()}`);
+    // console.log(`Letter ${i + 1} is ${logo[i].getTotalLength()}`);
 }
 
 const logo2 = document.querySelectorAll('#logo2 path');
-console.log(logo2.length);
+// console.log(logo2.length);
 
 for (let i = 0; i < logo2.length; i++) {
-    console.log(`Letter ${i + 1} is ${logo2[i].getTotalLength()}`);
+    // console.log(`Letter ${i + 1} is ${logo2[i].getTotalLength()}`);
 }
